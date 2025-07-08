bf052d9f13e4cc9f74beb770be0fe17d658df280
rokibulhasan <rokibulhasanutsoo@gmail.com>
2025-07-08 11:57:16 +0600

    update result feedback with new feature

--- src/pages/Result/ResultUpdatePage.jsx
+++ src/pages/Result/ResultUpdatePage.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import supabase from "../../supabase/config";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { enToBnNumber } from "../../utils/functions";
 import BackgroundBlurWrapper from "../../components/BackgroundBlurWrapper";
 import TextInput from "../../components/TextInput";
 import { secondTermExamSubjects } from "../../static/SecondTermExamRoutine";
 import LoadingComponent from "../../components/LoadingComponent";
 import Button from "../../components/Button";
-import { Loader } from "lucide-react";
+import {
+  CheckCircle2,
+  Edit,
+  File,
+  FilePen,
+  Home,
+  Loader,
+  Newspaper,
+} from "lucide-react";
 
 const ResultUpdatePage = () => {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const classCode = searchParams.get("c_c");
   const classLabel = searchParams.get("ccl");
   const subjectCode = searchParams.get("s_c");
@@ -22,8 +31,25 @@ const ResultUpdatePage = () => {
     query: false,
     submit: false,
   });
+
+  const tabObjects = {
+    open: false,
+    loading: false,
+    success: false,
+    error: false,
+    table: false,
+    editableTable: false,
+    title: "",
+  };
+
+  const [tab, setTab] = useState(tabObjects);
+
+  const handleTab = (obj = {}) => {
+    setTab((prev) => ({ ...prev, ...obj }));
+  };
+  const handleTabReset = () => setTab(tabObjects);
+
   const [query, setQuery] = useState([]);
-  const [submitData, setSubmitData] = useState([]);
 
   const getStudents = async () => {
     setLoading((prev) => ({ ...prev, students: true }));
@@ -71,7 +97,7 @@ const ResultUpdatePage = () => {
   }, []);
 
   const handleUpdateData = (studentId, subjectCode, marks) => {
-    console.log("call", studentId, subjectCode, marks);
+    // console.log("call", studentId, subjectCode, marks);
     const index = query.findIndex((item) => item.id === studentId);
 
     if (index !== -1) {
@@ -88,7 +114,11 @@ const ResultUpdatePage = () => {
   };
 
   const handleSubmit = async () => {
-    setLoading((prev) => ({ ...prev, submit: true }));
+    // setLoading((prev) => ({ ...prev, submit: true }));
+    handleTab({
+      open: true,
+      loading: true,
+    });
 
     const { data, error } = await supabase
       .from("resultTest")
@@ -97,19 +127,33 @@ const ResultUpdatePage = () => {
 
     if (error) {
       console.log(error);
+      handleTab({ error: true });
     }
 
     if (data) {
       setQuery(data);
+      handleTab({
+        success: true,
+        title: "আপডেটকৃত ফলাফল সমূহ",
+        table: true,
+      });
     }
 
-    setLoading((prev) => ({ ...prev, submit: false }));
+    // setLoading((prev) => ({ ...prev, submit: false }));
+    handleTab({ loading: false });
   };
 
-  // console.log(query);
+  const handleShowResult = () => {
+    handleTabReset();
+    handleTab({
+      open: true,
+      title: "ফলাফল সমূহ",
+      table: true,
+    });
+  };
 
   return (
-    <BackgroundBlurWrapper>
+    <BackgroundBlurWrapper className={"space-y-4"}>
       <div className="bg-gray-800 rounded-2xl ring-2 ring-gray-700 overflow-hidden">
         <div className="px-4 py-2 text-center">
           <p>পরীক্ষার ফলাফল যুক্ত করুন</p>
@@ -118,59 +162,182 @@ const ResultUpdatePage = () => {
         <p className="px-4 py-2.5 bg-gray-900 text-center">
           {secondTermExamSubjects[subjectCode]}
         </p>
+        <div className="flex justify-between p-2.5">
+          <div className="flex gap-4 items-center">
+            <Button
+              icon={FilePen}
+              iconClassName={"size-5"}
+              className={`p-2.5 ${!tab.open ? "bg-gray-700" : ""}`}
+              onClick={handleTabReset}
+            />
+            <Button
+              icon={File}
+              iconClassName={"size-5"}
+              className={`p-2.5 ${tab.table ? "bg-gray-700" : ""}`}
+              onClick={handleShowResult}
+            />
+          </div>
+          <Button
+            onClick={handleSubmit}
+            className={`bg-green-700 ring-green-900 active:scale-95 transition-transform ${
+              tab.loading ? "opacity-50" : ""
+            }`}
+          >
+            আপডেট করুন
+          </Button>
+        </div>
       </div>
+      {tab.open ? (
+        <div className="bg-gray-800 rounded-2xl ring-2 ring-gray-700 overflow-hidden">
+          {/* loading tab */}
+          {tab.loading && (
+            <div className="flex flex-col items-center my-2.5">
+              <Loader className="size-8 my-2.5 animate-spin text-green-400" />
+              <div className="text-center">
+                <p>ফলাফল গুলো আপডেট করা হচ্ছে</p>
+                <p>অনুগ্রহ করে অপেক্ষা করুন...</p>
+              </div>
+            </div>
+          )}
 
-      {}
-
-      <div className="space-y-4 my-4">
-        <LoadingComponent loadingState={loading.students}>
-          {data.students.map((data) => (
-            <div
-              key={data.id}
-              className="w-full ring-2 ring-gray-700 bg-gray-900 backdrop-blur-[6px] rounded-xl p-4 font-bangla"
-            >
-              <div className="grid grid-cols-5">
-                <div className="flex items-center gap-3 col-span-4">
-                  <p className="bg-gray-800 rounded-full ring-2 ring-gray-700 size-10 flex justify-center items-center">
-                    {enToBnNumber(data.roll)}
-                  </p>
-                  <div className="size-12">
-                    <img
-                      src={
-                        data?.studentImage ||
-                        "https://cdn-icons-png.flaticon.com/512/12225/12225935.png"
-                      }
-                      alt="Student Image"
-                      className="size-full bg-cover bg-top rounded"
-                    />
-                  </div>
-                  <p className="*:block">
-                    <span className="text-lg">{data.studentName}</span>
-                    {/* <span className="text-base">উপস্থিতিঃ ০%</span> */}
-                  </p>
+          {/* success tab */}
+          {tab.success && (
+            <div className="flex flex-col items-center my-5">
+              <div className="mb-10 flex flex-col items-center">
+                <CheckCircle2 className="size-12 my-2.5 text-green-400" />
+                <div className="text-center">
+                  <p>ফলাফলটি সফলভাবে আপডেট হয়েছে</p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-3.5">
+                <div className="flex gap-3.5">
+                  <Button
+                    className={
+                      "ring-green-900 active:scale-95 transition-transform"
+                    }
+                    icon={Home}
+                    iconClassName={"size-5"}
+                    value={"হোম"}
+                    onClick={() => navigate("/")}
+                  />
+                  <Button
+                    className={
+                      "w-full ring-green-900 active:scale-95 transition-transform"
+                    }
+                    icon={Edit}
+                    iconClassName={"size-5"}
+                    value={"এডিট করুন"}
+                    onClick={handleTabReset}
+                  />
                 </div>
                 <div>
-                  <TextInput
-                    className={"my-0"}
-                    type="number"
-                    inputClassName={
-                      "*:text-center bg-gray-950 *:ml-0 font-sans"
+                  <Button
+                    className={
+                      "w-full ring-green-900 active:scale-95 transition-transform"
                     }
-                    placeholder={"00"}
-                    defaultValue={
-                      query.find((item) => item.id === data.id)?.[
-                        subjectCode
-                      ] || ""
-                    }
-                    onChange={(e) =>
-                      handleUpdateData(
-                        data.id,
-                        subjectCode,
-                        parseInt(e.target.value || 0)
-                      )
-                    }
+                    icon={Newspaper}
+                    iconClassName={"size-5"}
+                    value={"নতুন ফলাফল তৈরি করুন"}
+                    onClick={() => navigate("/results")}
                   />
-                  {/* <input
+                </div>
+              </div>
+            </div>
+          )}
+
+          <p className="text-center mt-5 mb-2.5">{tab.title}</p>
+
+          {/* table tab */}
+          {tab.table && (
+            <div className="px-4 mb-4">
+              <table className="w-full">
+                <tbody className="*:odd:bg-gray-900 **:my-2.5 *:border  *:border-gray-700">
+                  <tr className="text-center *:py-2.5 *:px-1.5">
+                    <td>রোল</td>
+                    <td>ছবি</td>
+                    <td className="text-start !pl-3.5">নাম</td>
+                    <td>প্রাপ্ত নম্বর</td>
+                  </tr>
+                  {data.students.map((data) => (
+                    <tr className="text-center">
+                      <td>{enToBnNumber(data.roll)}</td>
+                      <td className="px-0.5">
+                        <div className="size-12">
+                          <img
+                            src={
+                              data?.studentImage ||
+                              "https://cdn-icons-png.flaticon.com/512/12225/12225935.png"
+                            }
+                            alt="Student Image"
+                            className="size-full bg-cover bg-top rounded"
+                          />
+                        </div>
+                      </td>
+                      <td className="text-start pl-3.5">{data.studentName}</td>
+                      <td>
+                        {enToBnNumber(
+                          query.find((item) => item.id === data.id)?.[
+                            subjectCode
+                          ] || "00"
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="space-y-4">
+          <LoadingComponent loadingState={loading.students}>
+            {data.students.map((data) => (
+              <div
+                key={data.id}
+                className="w-full ring-2 ring-gray-700 bg-gray-900 backdrop-blur-[6px] rounded-xl p-4 font-bangla"
+              >
+                <div className="grid grid-cols-5">
+                  <div className="flex items-center gap-3 col-span-4">
+                    <p className="bg-gray-800 rounded-full ring-2 ring-gray-700 size-10 flex justify-center items-center">
+                      {enToBnNumber(data.roll)}
+                    </p>
+                    <div className="size-12">
+                      <img
+                        src={
+                          data?.studentImage ||
+                          "https://cdn-icons-png.flaticon.com/512/12225/12225935.png"
+                        }
+                        alt="Student Image"
+                        className="size-full bg-cover bg-top rounded"
+                      />
+                    </div>
+                    <p className="*:block">
+                      <span className="text-lg">{data.studentName}</span>
+                      {/* <span className="text-base">উপস্থিতিঃ ০%</span> */}
+                    </p>
+                  </div>
+                  <div>
+                    <TextInput
+                      className={"my-0"}
+                      type="number"
+                      inputClassName={
+                        "*:text-center bg-gray-950 *:ml-0 font-sans"
+                      }
+                      placeholder={"00"}
+                      defaultValue={
+                        query.find((item) => item.id === data.id)?.[
+                          subjectCode
+                        ] || ""
+                      }
+                      onChange={(e) =>
+                        handleUpdateData(
+                          data.id,
+                          subjectCode,
+                          parseInt(e.target.value || 0)
+                        )
+                      }
+                    />
+                    {/* <input
                     type="number"
                     className="border w-full font-sans p-2 text-center rounded-xl"
                     placeholder={"00"}
@@ -187,28 +354,29 @@ const ResultUpdatePage = () => {
                       )
                     }
                   /> */}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-          <div>
-            <Button
-              // value={"আপডেট করুন"}
-              className={"w-full py-5 my-10"}
-              onClick={handleSubmit}
-            >
-              {loading.submit ? (
-                <div className="flex items-center justify-center gap-2.5">
-                  <Loader className="size-7 animate-spin" />{" "}
-                  <span>আপডেট হচ্ছে</span>
-                </div>
-              ) : (
-                "আপডেট করুন"
-              )}
-            </Button>
-          </div>
-        </LoadingComponent>
-      </div>
+            ))}
+            {/* <div>
+              <Button
+                // value={"আপডেট করুন"}
+                className={"w-full py-5 my-10"}
+                onClick={handleSubmit}
+              >
+                {loading.submit ? (
+                  <div className="flex items-center justify-center gap-2.5">
+                    <Loader className="size-7 animate-spin" />{" "}
+                    <span>আপডেট হচ্ছে</span>
+                  </div>
+                ) : (
+                  "আপডেট করুন"
+                )}
+              </Button>
+            </div> */}
+          </LoadingComponent>
+        </div>
+      )}
     </BackgroundBlurWrapper>
   );
 };
